f5913b8f1c9bdc33f62ed2261bb13fabcc380b25
Michel5925 <219335338+Michel5925@users.noreply.github.com>
2026-07-16 17:33:02 +0100

    Added operate function

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let number1;
+let operator;
+let number2;
+
+function operate(operator, number1, number2) {
+    if(operator == "+")
+    {
+        return add(number1, number2);
+    }
+
+    else if(operator == "-")
+    {
+        return subtract(number1, number2);
+    }
+
+    else if(operator == "*")
+    {
+        return multiply(number1, number2);
+    }
+
+    else if(operator == "/")
+    {
+        return divide(number1, number2);
+    }
+}
